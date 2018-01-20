f4740ef11dc0d7fd92d291465959d26daa46aa95
guiga <gmbo182@hotmail.com>
2018-01-20 13:44:53 -0300

    add expires and corrections

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const findType = value =>
     .toLowerCase());
 
 const set = db => async (key, value) => {
+  if (!key) return false;
   const type = findType(value);
   if (type === "string") return db.set(key, value);
   else if (type === "object") return db.hset(key, value);
@@ -12,28 +13,30 @@ const set = db => async (key, value) => {
 };
 
 const get = db => async (key, value) => {
+  if (!key) return false;
   const type = findType(value);
   if (type === "array") return db.hmget(key, value);
-  else if (type === "string") return db.get(key);
-  else return false;
+  else return db.get(key);
 };
 
 const remove = db => async (key, value) => {
+  if (!key) return false;
   const type = findType(value);
   if (type === "array") return db.hdel(key, value);
-  else if (type === "string") return db.del(key);
-  else return false;
+  else return db.del(key);
 };
 
-const has = db => async key => db.exists(key);
+const has = db => async key => (key ? db.exists(key) : false);
 
-const adapter = db => {
-  return {
-    set: set(db),
-    get: get(db),
-    has: has(db),
-    remove: remove(db)
-  };
-};
+const expires = db => async (key, time) =>
+  key && time ? db.pexpire(key, time) : false;
+
+const adapter = db => ({
+  set: set(db),
+  get: get(db),
+  has: has(db),
+  remove: remove(db),
+  expires: expires(db)
+});
 
 module.exports = adapter;
